abad6734ebd97ad9f5dade9a8c14f430a369dfaa
Martin Molinero <martin.molinero1@gmail.com>
2018-08-07 11:45:08 -0300

    Fix for Quandl custom data race condition

--- Common/Data/Custom/Quandl.cs
+++ Common/Data/Custom/Quandl.cs
@@ -1,11 +1,11 @@
 ﻿/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,7 +20,7 @@ using System.Globalization;
 namespace QuantConnect.Data.Custom
 {
     /// <summary>
-    /// Quandl Data Type - Import generic data from quandl, without needing to define Reader methods. 
+    /// Quandl Data Type - Import generic data from quandl, without needing to define Reader methods.
     /// This reads the headers of the data imported, and dynamically creates properties for the imported data.
     /// </summary>
     public class Quandl : DynamicData
@@ -126,7 +126,9 @@ namespace QuantConnect.Data.Custom
         /// <returns>STRING API Url for Quandl.</returns>
         public override SubscriptionDataSource GetSource(SubscriptionDataConfig config, DateTime date, bool isLiveMode)
         {
-            var source = @"https://www.quandl.com/api/v3/datasets/" + config.Symbol.Value + ".csv?order=asc&api_key=" + _authCode;
+            var source = @"https://www.quandl.com/api/v3/datasets/"
+                         + $"{config.Symbol.Value}.csv?order=asc&api_key={_authCode}"
+                         + $"&start_date={date.Subtract(TimeSpan.FromDays(1)):yyyy-MM-dd}";
             return new SubscriptionDataSource(source, SubscriptionTransportMedium.RemoteFile);
         }
 
